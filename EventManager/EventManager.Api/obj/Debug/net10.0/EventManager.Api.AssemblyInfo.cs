db3bd24600e1aef3398391f42f9cc415c766f97d
Dennis Rotar <dennisrotar@gmail.com>
2026-04-02 13:36:13 +0500

    Добавлен файл README.md с инструкциями по установке.

--- EventManager/EventManager.Api/obj/Debug/net10.0/EventManager.Api.AssemblyInfo.cs
+++ EventManager/EventManager.Api/obj/Debug/net10.0/EventManager.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1193307c2938766a0bb7eef817e385815c863e50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd07f58e8ee04c0b45ba33e8044a739d3ce92c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
